3db99accd0af8cbc823e3837ec9f3f328576d419
MeiyuanXiang <meiguixiangmanyuan@163.com>
2021-08-16 13:57:11 +0800

    新增FAST_LIO_SLAM（包括FAST-LIO2和SC-PGO）中文注释的代码

---
+++ src/FAST_LIO_SLAM/FAST-LIO/src/IMU_Processing.hpp
@@ -0,0 +1,396 @@
+#include <cmath>
+#include <math.h>
+#include <deque>
+#include <mutex>
+#include <thread>
+#include <fstream>
+#include <csignal>
+#include <ros/ros.h>
+#include <so3_math.h>
+#include <Eigen/Eigen>
+#include <common_lib.h>
+#include <pcl/common/io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <condition_variable>
+#include <nav_msgs/Odometry.h>
+#include <pcl/common/transforms.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <tf/transform_broadcaster.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Vector3.h>
+#include "use-ikfom.hpp"
+
+/// *************Preconfiguration
+
+#define MAX_INI_COUNT (20)
+
+const bool time_list(PointType &x, PointType &y) { return (x.curvature < y.curvature); };
+
+/// *************IMU Process and undistortion
+class ImuProcess
+{
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  ImuProcess();
+  ~ImuProcess();
+
+  void Reset();
+  void Reset(double start_timestamp, const sensor_msgs::ImuConstPtr &lastimu);
+  void set_extrinsic(const V3D &transl, const M3D &rot);
+  void set_extrinsic(const V3D &transl);
+  void set_extrinsic(const MD(4, 4) & T);
+  void set_gyr_cov(const V3D &scaler);
+  void set_acc_cov(const V3D &scaler);
+  void set_gyr_bias_cov(const V3D &b_g);
+  void set_acc_bias_cov(const V3D &b_a);
+  Eigen::Matrix<double, 12, 12> Q;
+  void Process(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, PointCloudXYZI::Ptr pcl_un_);
+
+  ofstream fout_imu;
+  V3D cov_acc;
+  V3D cov_gyr;
+  V3D cov_acc_scale;
+  V3D cov_gyr_scale;
+  V3D cov_bias_gyr;
+  V3D cov_bias_acc;
+  double first_lidar_time;
+
+private:
+  void IMU_init(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, int &N);
+  void UndistortPcl(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, PointCloudXYZI &pcl_in_out);
+
+  PointCloudXYZI::Ptr cur_pcl_un_;
+  sensor_msgs::ImuConstPtr last_imu_;
+  deque<sensor_msgs::ImuConstPtr> v_imu_;
+  vector<Pose6D> IMUpose;
+  vector<M3D> v_rot_pcl_;
+  M3D Lidar_R_wrt_IMU;
+  V3D Lidar_T_wrt_IMU;
+  V3D mean_acc;
+  V3D mean_gyr;
+  V3D angvel_last;
+  V3D acc_s_last;
+  double start_timestamp_;
+  double last_lidar_end_time_;
+  int init_iter_num = 1;
+  bool b_first_frame_ = true;
+  bool imu_need_init_ = true;
+};
+
+ImuProcess::ImuProcess()
+    : b_first_frame_(true), imu_need_init_(true), start_timestamp_(-1)
+{
+  init_iter_num = 1;
+  Q = process_noise_cov();
+  cov_acc = V3D(0.1, 0.1, 0.1);
+  cov_gyr = V3D(0.1, 0.1, 0.1);
+  cov_bias_gyr = V3D(0.0001, 0.0001, 0.0001);
+  cov_bias_acc = V3D(0.0001, 0.0001, 0.0001);
+  mean_acc = V3D(0, 0, -1.0);
+  mean_gyr = V3D(0, 0, 0);
+  angvel_last = Zero3d;
+  Lidar_T_wrt_IMU = Zero3d;
+  Lidar_R_wrt_IMU = Eye3d;
+  last_imu_.reset(new sensor_msgs::Imu());
+}
+
+ImuProcess::~ImuProcess() {}
+
+void ImuProcess::Reset()
+{
+  mean_acc = V3D(0, 0, -1.0);
+  mean_gyr = V3D(0, 0, 0);
+  angvel_last = Zero3d;
+  imu_need_init_ = true;
+  start_timestamp_ = -1;
+  init_iter_num = 1;
+  v_imu_.clear();
+  IMUpose.clear();
+  last_imu_.reset(new sensor_msgs::Imu());
+  cur_pcl_un_.reset(new PointCloudXYZI());
+}
+
+void ImuProcess::set_extrinsic(const MD(4, 4) & T)
+{
+  Lidar_T_wrt_IMU = T.block<3, 1>(0, 3);
+  Lidar_R_wrt_IMU = T.block<3, 3>(0, 0);
+}
+
+void ImuProcess::set_extrinsic(const V3D &transl)
+{
+  Lidar_T_wrt_IMU = transl;
+  Lidar_R_wrt_IMU.setIdentity();
+}
+
+void ImuProcess::set_extrinsic(const V3D &transl, const M3D &rot)
+{
+  Lidar_T_wrt_IMU = transl;
+  Lidar_R_wrt_IMU = rot;
+}
+
+void ImuProcess::set_gyr_cov(const V3D &scaler)
+{
+  cov_gyr_scale = scaler;
+}
+
+void ImuProcess::set_acc_cov(const V3D &scaler)
+{
+  cov_acc_scale = scaler;
+}
+
+void ImuProcess::set_gyr_bias_cov(const V3D &b_g)
+{
+  cov_bias_gyr = b_g;
+}
+
+void ImuProcess::set_acc_bias_cov(const V3D &b_a)
+{
+  cov_bias_acc = b_a;
+}
+
+void ImuProcess::IMU_init(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, int &N)
+{
+  /** 1. initializing the gravity, gyro bias, acc and gyro covariance
+   ** 2. normalize the acceleration measurenments to unit gravity **/
+
+  V3D cur_acc, cur_gyr;
+
+  if (b_first_frame_)
+  {
+    Reset();
+    N = 1;
+    b_first_frame_ = false;
+    const auto &imu_acc = meas.imu.front()->linear_acceleration;
+    const auto &gyr_acc = meas.imu.front()->angular_velocity;
+    mean_acc << imu_acc.x, imu_acc.y, imu_acc.z;
+    mean_gyr << gyr_acc.x, gyr_acc.y, gyr_acc.z;
+    first_lidar_time = meas.lidar_beg_time;
+  }
+
+  for (const auto &imu : meas.imu)
+  {
+    const auto &imu_acc = imu->linear_acceleration;
+    const auto &gyr_acc = imu->angular_velocity;
+    cur_acc << imu_acc.x, imu_acc.y, imu_acc.z;
+    cur_gyr << gyr_acc.x, gyr_acc.y, gyr_acc.z;
+
+    mean_acc += (cur_acc - mean_acc) / N;
+    mean_gyr += (cur_gyr - mean_gyr) / N;
+
+    // cwiseProduct：输出相同位置的两个矩阵中各个系数的乘积所组成的矩阵
+    cov_acc = cov_acc * (N - 1.0) / N + (cur_acc - mean_acc).cwiseProduct(cur_acc - mean_acc) * (N - 1.0) / (N * N); // 线加速度协方差
+    cov_gyr = cov_gyr * (N - 1.0) / N + (cur_gyr - mean_gyr).cwiseProduct(cur_gyr - mean_gyr) * (N - 1.0) / (N * N); // 重力加速度协方差
+
+    N++;
+  }
+  
+  state_ikfom init_state = kf_state.get_x();
+  init_state.grav = S2(-mean_acc / mean_acc.norm() * G_m_s2);
+
+  //state_inout.rot = Eye3d; // Exp(mean_acc.cross(V3D(0, 0, -1 / scale_gravity)));
+  init_state.bg = mean_gyr;
+  init_state.offset_T_L_I = Lidar_T_wrt_IMU;
+  init_state.offset_R_L_I = Lidar_R_wrt_IMU;
+  kf_state.change_x(init_state);
+
+  esekfom::esekf<state_ikfom, 12, input_ikfom>::cov init_P = kf_state.get_P();
+  init_P.setIdentity();
+  init_P(6, 6) = init_P(7, 7) = init_P(8, 8) = 0.00001;
+  init_P(9, 9) = init_P(10, 10) = init_P(11, 11) = 0.00001;
+  init_P(15, 15) = init_P(16, 16) = init_P(17, 17) = 0.0001;
+  init_P(18, 18) = init_P(19, 19) = init_P(20, 20) = 0.001;
+  init_P(21, 21) = init_P(22, 22) = 0.00001;
+  kf_state.change_P(init_P);
+  last_imu_ = meas.imu.back();
+}
+
+// 点云运动补偿，假设第一个点为基准，使用IMU旋转补偿激光点云
+void ImuProcess::UndistortPcl(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, PointCloudXYZI &pcl_out)
+{
+  /*
+    IMU预积分
+    把堆积的IMU数据进行预积分处理，推导出每个IMU数据时刻相对于全局坐标系的T，以及对应的方差。
+    每次开始之前，都会把上一次预积分的最后一个IMU加入到本次IMU队列的头部，以及对应的姿态结果作为此次预积分的开始姿态。
+    这种推理会导致IMU推理的轨迹飞的很快，但是不影响我们总的结果，因为我们取的是短时间内的相对运动姿态。
+    这里IMU的预积分使用中值积分。
+  */
+
+  /* 数据准备 */
+
+  /*** add the imu of the last frame-tail to the of current frame-head ***/
+  auto v_imu = meas.imu;
+  v_imu.push_front(last_imu_); // 让IMU的时间能包住Lidar的时间
+  const double &imu_beg_time = v_imu.front()->header.stamp.toSec();
+  const double &imu_end_time = v_imu.back()->header.stamp.toSec();
+  const double &pcl_beg_time = meas.lidar_beg_time;
+
+  /*** sort point clouds by offset time ***/
+  pcl_out = *(meas.lidar);
+  sort(pcl_out.points.begin(), pcl_out.points.end(), time_list);
+  const double &pcl_end_time = pcl_beg_time + pcl_out.points.back().curvature / double(1000);
+
+  /*** Initialize IMU pose ***/
+  state_ikfom imu_state = kf_state.get_x();
+  IMUpose.clear();
+  IMUpose.push_back(set_pose6d(0.0, acc_s_last, angvel_last, imu_state.vel, imu_state.pos, imu_state.rot.toRotationMatrix()));
+
+  /* 预积分过程 */
+
+  /*** forward propagation at each imu point ***/
+  V3D angvel_avr, acc_avr, acc_imu, vel_imu, pos_imu;
+  M3D R_imu;
+
+  double dt = 0;
+
+  input_ikfom in;
+  for (auto it_imu = v_imu.begin(); it_imu < (v_imu.end() - 1); it_imu++)
+  {
+    // 中值积分
+    auto &&head = *(it_imu);
+    auto &&tail = *(it_imu + 1);
+
+    if (tail->header.stamp.toSec() < last_lidar_end_time_)
+      continue;
+
+    angvel_avr << 0.5 * (head->angular_velocity.x + tail->angular_velocity.x),
+        0.5 * (head->angular_velocity.y + tail->angular_velocity.y),
+        0.5 * (head->angular_velocity.z + tail->angular_velocity.z);
+    acc_avr << 0.5 * (head->linear_acceleration.x + tail->linear_acceleration.x),
+        0.5 * (head->linear_acceleration.y + tail->linear_acceleration.y),
+        0.5 * (head->linear_acceleration.z + tail->linear_acceleration.z);
+
+    acc_avr = acc_avr * G_m_s2 / mean_acc.norm(); // - state_inout.ba;
+
+    if (head->header.stamp.toSec() < last_lidar_end_time_)
+    {
+      dt = tail->header.stamp.toSec() - last_lidar_end_time_;
+    }
+    else
+    {
+      dt = tail->header.stamp.toSec() - head->header.stamp.toSec();
+    }
+
+    in.acc = acc_avr;
+    in.gyro = angvel_avr;
+    Q.block<3, 3>(0, 0).diagonal() = cov_gyr;
+    Q.block<3, 3>(3, 3).diagonal() = cov_acc;
+    Q.block<3, 3>(6, 6).diagonal() = cov_bias_gyr;
+    Q.block<3, 3>(9, 9).diagonal() = cov_bias_acc;
+    kf_state.predict(dt, Q, in);
+
+    /* save the poses at each IMU measurements */
+    imu_state = kf_state.get_x();
+    angvel_last = angvel_avr - imu_state.bg;
+    acc_s_last = imu_state.rot * (acc_avr - imu_state.ba);
+    for (int i = 0; i < 3; i++)
+    {
+      acc_s_last[i] += imu_state.grav[i];
+    }
+
+    double &&offs_t = tail->header.stamp.toSec() - pcl_beg_time;
+    IMUpose.push_back(set_pose6d(offs_t, acc_s_last, angvel_last, imu_state.vel, imu_state.pos, imu_state.rot.toRotationMatrix()));
+  }
+
+  /*** calculated the pos and attitude prediction at the frame-end ***/
+  double note = pcl_end_time > imu_end_time ? 1.0 : -1.0;
+  dt = note * (pcl_end_time - imu_end_time);
+  kf_state.predict(dt, Q, in);
+
+  imu_state = kf_state.get_x();
+  last_imu_ = meas.imu.back();
+  last_lidar_end_time_ = pcl_end_time;
+
+  /* 点云补偿 */
+
+  /*** undistort each lidar point (backward propagation) ***/
+  auto it_pcl = pcl_out.points.end() - 1;
+  for (auto it_kp = IMUpose.end() - 1; it_kp != IMUpose.begin(); it_kp--)
+  {
+    auto head = it_kp - 1;
+    auto tail = it_kp;
+    R_imu << MAT_FROM_ARRAY(head->rot);
+    vel_imu << VEC_FROM_ARRAY(head->vel);
+    pos_imu << VEC_FROM_ARRAY(head->pos);
+    acc_imu << VEC_FROM_ARRAY(tail->acc);
+    angvel_avr << VEC_FROM_ARRAY(tail->gyr);
+
+    for (; it_pcl->curvature / double(1000) > head->offset_time; it_pcl--)
+    {
+      dt = it_pcl->curvature / double(1000) - head->offset_time;
+
+      /* Transform to the 'end' frame, using only the rotation
+       * Note: Compensation direction is INVERSE of Frame's moving direction
+       * So if we want to compensate a point at timestamp-i to the frame-e
+       * P_compensate = R_imu_e ^ T * (R_i * P_i + T_ei) where T_ei is represented in global frame */
+      M3D R_i(R_imu * Exp(angvel_avr, dt));
+
+      V3D P_i(it_pcl->x, it_pcl->y, it_pcl->z);
+      V3D T_ei(pos_imu + vel_imu * dt + 0.5 * acc_imu * dt * dt - imu_state.pos);
+      V3D P_compensate = imu_state.offset_R_L_I.conjugate() * (imu_state.rot.conjugate() * (R_i * (imu_state.offset_R_L_I * P_i + imu_state.offset_T_L_I) + T_ei) - imu_state.offset_T_L_I); // not accurate!
+      // T_ei表示i时刻的lidar坐标原点到end时刻的坐标平移量，用的全局坐标系的衡量
+      // T_ei=Ti-Te
+      // 为什么是Ti-Te，在global坐标系下原点为o，Ti是向量oi,Te是向量oe,
+      // Ti-Te就是向量oi-oe，得到的是向量ei,
+      // 即以e指向i的向量，以e为原点的向量
+      // end时刻已经计算出来了就是pos_liD_e
+      // i时刻的坐标计算方式与pos_liD_e一样
+      // pos_liD_i=Pos_i+rot_i*Lidar_offset_to_IMU
+      // 而Pos_i=Pos_head+velocity_head*dt+0.5*acc_head*dt*dt
+
+      // save Undistorted points and their rotation
+      it_pcl->x = P_compensate(0);
+      it_pcl->y = P_compensate(1);
+      it_pcl->z = P_compensate(2);
+
+      if (it_pcl == pcl_out.points.begin())
+        break;
+    }
+  }
+}
+
+void ImuProcess::Process(const MeasureGroup &meas, esekfom::esekf<state_ikfom, 12, input_ikfom> &kf_state, PointCloudXYZI::Ptr cur_pcl_un_)
+{
+  double t1, t2, t3;
+  t1 = omp_get_wtime();
+
+  if (meas.imu.empty())
+  {
+    return;
+  };
+
+  ROS_ASSERT(meas.lidar != nullptr);
+
+  if (imu_need_init_)
+  {
+    // The very first lidar frame
+    IMU_init(meas, kf_state, init_iter_num);
+
+    imu_need_init_ = true;
+
+    last_imu_ = meas.imu.back();
+
+    state_ikfom imu_state = kf_state.get_x();
+    if (init_iter_num > MAX_INI_COUNT)
+    {
+      cov_acc *= pow(G_m_s2 / mean_acc.norm(), 2);
+      imu_need_init_ = false;
+
+      cov_acc = cov_acc_scale;
+      cov_gyr = cov_gyr_scale;
+      ROS_INFO("IMU Initial Done");
+      fout_imu.open(DEBUG_FILE_DIR("imu.txt"), ios::out);
+    }
+
+    return;
+  }
+
+  UndistortPcl(meas, kf_state, *cur_pcl_un_);
+
+  t2 = omp_get_wtime();
+  t3 = omp_get_wtime();
+}
---
+++ src/FAST_LIO_SLAM/FAST-LIO/src/preprocess.h
@@ -0,0 +1,146 @@
+#include <ros/ros.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <livox_ros_driver/CustomMsg.h>
+
+using namespace std;
+
+#define IS_VALID(a) ((abs(a) > 1e8) ? true : false) // 没用到
+
+typedef pcl::PointXYZINormal PointType;
+typedef pcl::PointCloud<PointType> PointCloudXYZI;
+
+enum LID_TYPE
+{
+  AVIA = 1,
+  VELO16,
+  OUST64
+}; //{1, 2, 3} 支持的雷达类型
+enum Feature
+{
+  Nor,
+  Poss_Plane,
+  Real_Plane,
+  Edge_Jump,
+  Edge_Plane,
+  Wire,
+  ZeroPoint
+}; // 特征类型：正常、可能的平面点、确定的平面点、有跨越的边、边上的平面点、线段、无效点
+enum Surround
+{
+  Prev,
+  Next
+}; // 位置标识：前一个、后一个
+enum E_jump
+{
+  Nr_nor,
+  Nr_zero,
+  Nr_180,
+  Nr_inf,
+  Nr_blind
+}; // 有跨越边的类型：正常、0、180、无穷大、最大值
+
+struct orgtype
+{
+  double range;     // 和邻近点的点距，用以确定是否为重复点，默认为当前点的x坐标，但是最后一个点取xy平面向量的模
+  double dista;     // 当前点与最后一个点距离的平方
+  double angle[2];  // 当前点与前一个点的夹角、当前点与后一个点的夹角
+  double intersect; // 前一个点和后一个点夹角
+  E_jump edj[2];    // 前后两点的类型
+  Feature ftype;    // 点类型
+  orgtype()
+  {
+    range = 0;
+    edj[Prev] = Nr_nor;
+    edj[Next] = Nr_nor;
+    ftype = Nor;
+    intersect = 2;
+  }
+};
+
+// velodyne数据结构
+namespace velodyne_ros
+{
+  struct EIGEN_ALIGN16 Point
+  {
+    PCL_ADD_POINT4D;                // 4D点坐标类型
+    float intensity;                // 强度
+    float time;                     // 时间
+    uint16_t ring;                  // 点所属的圈数
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW // 进行内存对齐
+  };
+} // namespace velodyne_ros
+
+// 注册velodyne_ros的Point类型
+POINT_CLOUD_REGISTER_POINT_STRUCT(velodyne_ros::Point,
+                                  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, time, time)(uint16_t, ring, ring))
+
+// ouster数据结构
+namespace ouster_ros
+{
+  struct EIGEN_ALIGN16 Point
+  {
+    PCL_ADD_POINT4D;                // 4D点坐标类型
+    float intensity;                // 强度
+    uint32_t t;                     // 时间
+    uint16_t reflectivity;          // 反射率
+    uint8_t ring;                   // 点所属的圈数
+    uint16_t ambient;               // 没用到
+    uint32_t range;                 // 距离
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW // 进行内存对齐
+  };
+} // namespace ouster_ros
+
+// clang-format off
+POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    // use std::uint32_t to avoid conflicting with pcl::uint32_t
+    (std::uint32_t, t, t)
+    (std::uint16_t, reflectivity, reflectivity)
+    (std::uint8_t, ring, ring)
+    (std::uint16_t, ambient, ambient)
+    (std::uint32_t, range, range)
+)
+
+class Preprocess
+{
+  public:
+
+  Preprocess();
+  ~Preprocess();
+  
+  void process(const livox_ros_driver::CustomMsg::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
+  void process(const sensor_msgs::PointCloud2::ConstPtr &msg, PointCloudXYZI::Ptr &pcl_out);
+  void set(bool feat_en, int lid_type, double bld, int pfilt_num);
+
+  PointCloudXYZI pl_full, pl_corn, pl_surf; // 全部点、边缘点、平面点
+  PointCloudXYZI pl_buff[128]; //maximum 128 line lidar
+  vector<orgtype> typess[128]; //maximum 128 line lidar
+  int lidar_type, point_filter_num, N_SCANS; // 雷达类型、采样间隔、扫描线数
+  double blind; // 最小距离阈值
+  bool feature_enabled, given_offset_time; // 是否提取特征、是否进行时间偏移
+  ros::Publisher pub_full, pub_surf, pub_corn; // 发布全部点、发布平面点、发布边缘点
+
+  private:
+  void avia_handler(const livox_ros_driver::CustomMsg::ConstPtr &msg);
+  void oust64_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  void velodyne_handler(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  void give_feature(PointCloudXYZI &pl, vector<orgtype> &types);
+  void pub_func(PointCloudXYZI &pl, const ros::Time &ct);
+  int  plane_judge(const PointCloudXYZI &pl, vector<orgtype> &types, uint i, uint &i_nex, Eigen::Vector3d &curr_direct);
+  bool small_plane(const PointCloudXYZI &pl, vector<orgtype> &types, uint i_cur, uint &i_nex, Eigen::Vector3d &curr_direct); // 没有用到
+  bool edge_jump_judge(const PointCloudXYZI &pl, vector<orgtype> &types, uint i, Surround nor_dir);
+  
+  int group_size;
+  double disA, disB, inf_bound;
+  double limit_maxmid, limit_midmin, limit_maxmin;
+  double p2l_ratio;
+  double jump_up_limit, jump_down_limit;
+  double cos160;
+  double edgea, edgeb;
+  double smallp_intersect, smallp_ratio;
+  double vx, vy, vz;
+};
